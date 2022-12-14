a9b2105e9dd876213272de80addd62a32b551c16
yim taehyeon <119641712+th0267@users.noreply.github.com>
2022-12-15 00:06:11 +0900

    Add files via upload

---
+++ 221214.cpp
@@ -0,0 +1,46 @@
+/*#include <iostream>
+using namespace std;
+
+int main(){
+    float a =1.5;
+    float b = 2.7;
+    float add = a+b;
+    float sub = a-b;
+
+    cout<< add<<sub;
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main(){
+ double a,b,c;
+ cin>>a>>b;
+ c= a+b;
+ cout<<c;
+ return 0;
+}
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    double BMI;
+    double kg;
+    double m;
+    cout<<"체중(kg)입력:";
+    cin>>kg;
+    cout<<"키(m)입력:";
+    cin>>m;
+    BMI=kg/pow(m,2);
+    cout<<BMI;
+    return 0;
+}
+#include <iostream>
+using namespace std;
+int main(){
+    char str,strr;
+    cin>>str;
+    strr=str+1;
+    cout<<strr;
+}*/
